f6ef7f23256cbce587fc2b69f625b8556d11b3fd
Raphael Taylor <rafikitaylor828@gmail.com>
2026-06-02 00:29:34 +0000

    fixed axios

--- src/api/axios.ts
+++ src/api/axios.ts
@@ -1,7 +1,8 @@
 import axios from 'axios';
 
+const API_URL = import.meta.env.VITE_API_URL?.replace(/\/+$/, '') ?? '';
 const API = axios.create({
-  baseURL: 'http://localhost:5000/api',
+  baseURL: `${API_URL}/api`,
 });
 
 API.interceptors.request.use((config) => {
